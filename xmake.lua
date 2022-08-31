f90fe8eb98359f40950188625de7980adfda689a
Zmmfly <shenqhy@qq.com>
2022-08-31 17:51:34 +0800

    update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,15 +1,16 @@
 add_requires("spdlog", "fmt")
 set_targetdir("dist")
 
--- if is_plat("windows") then
---     add_syslinks("pthread")
--- end
+if is_plat("linux") or is_plat("mingw") then
+    add_syslinks("pthread")
+end
 
-target("PMD")
+target("MPD")
     set_kind("static")
     add_files("src/**.cc")
     add_packages("spdlog", "fmt", {public=true})
     add_includedirs("include", {public=true})
-    set_languages("c++17")
+    set_languages("c++20")
+    set_optimize("fastest")
 
 includes("examples")
